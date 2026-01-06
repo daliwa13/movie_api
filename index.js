ec45728cf19e16c124b7357e29b1c8a036bc3297
Iwona Daleka <iwona.daleka@gmail.com>
2026-01-06 11:59:50 +0100

    Updated listen port.

--- index.js
+++ index.js
@@ -355,6 +355,7 @@ app.use((err, req, res, next) => {
 });
 
 // listen for requests
-app.listen(8080, () => {
-    console.log('Your app is listening on port 8080.');
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Your app is listening on port ${PORT}.`);
 });
